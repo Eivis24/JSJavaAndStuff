609c592172d1baf41c64b5121e392e25b9675dbf
Eivis24 <tyrasvyras@gmail.com>
2018-11-20 12:23:39 +0200

    So i made an index, a basic app, which prints a hello world and a styles file. I've also icluded some react related libraries

---
+++ antraPaskaita/modulis/app.js
@@ -0,0 +1,13 @@
+console.log('Pasileido')
+
+for (var i = 0; i < 2; i++) {
+	console.log('Pasileido ' + i)
+}
+
+
+const component = <h1>Hello, world</h1>;
+
+ReactDOM.render(
+	component,
+	document.getElementById('root')
+);
